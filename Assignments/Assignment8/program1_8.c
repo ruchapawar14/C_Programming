9036fb337a17ee009ec8d88497c5e0a7d5d578ce
Rucha Pawar <ruchap2070@gmail.com>
2026-01-03 15:50:24 +0530

    Create program1_8.c

---
+++ Assignments/Assignment8/program1_8.c
@@ -0,0 +1,42 @@
+////////////////////////////////////////
+//
+// Function Name : Number 
+// Description   : Write a program which accept number from user and if number is less than 50 then 
+//                 print small,if it is greater than 50 and less than 100 then print medium,if it is greater
+//                 than 100 then print large.
+// Input         : Integer
+// Output        : None
+// Author        : Rucha Pawar
+// Date          : 05/12/2025
+//
+////////////////////////////////////////
+
+#include<stdio.h>
+
+void Number(int iNo)
+{
+    if(iNo < 50)
+    {
+        printf("Small\n");
+    }
+    else if((iNo >= 50) && (iNo <= 100))
+    {
+        printf("Medium\n");
+    }
+    else
+    {
+        printf("Large\n");
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter a number : ");
+    scanf("%d", &iValue);
+
+    Number(iValue);
+
+    return 0;
+}
